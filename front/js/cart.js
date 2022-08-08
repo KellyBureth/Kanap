477fa56a968a52be0dea8b9b68b9df04ae39bec7
Kelly <cordinette@outlook.fr>
2022-08-08 21:23:19 +0200

    Final version with kanap icon

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,11 +7,65 @@ let colorProduct = '';
 let priceProduct = '';
 let quantity ='';
 
+
 /*________RETRIEVE PRODUCTS ON LOCAL STORAGE________*/
 let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
 
-/*________CREATE THE FONCTION TO ADD UP VALUES OF AN ARRAY________*/
+
+/*________CREATE THE FUNCTION TO ADD UP VALUES OF AN ARRAY________*/
 let reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+
+/*________CREATE AND STYLING ELEMENTS FOR SVG THAT DISPLAYS QUANTITY OF PRODUCTS ON CART________*/
+const nav = document.querySelector('.limitedWidthBlock nav ul');
+let totalQuantity = ''
+const divIcone = document.createElement('div');
+nav.appendChild(divIcone);
+const quantityInCard = document.createElement('p');
+const iconeKanap = document.createElement('p');
+divIcone.appendChild(quantityInCard);
+divIcone.appendChild(iconeKanap);
+divIcone.style.alignSelf = 'center';
+divIcone.style.alignItems = 'center';
+divIcone.style.border = '0.1rem dotted #3498db';
+divIcone.style.borderRadius = '1rem';
+divIcone.style.height = '1.9rem';
+divIcone.style.width = '1.7rem';
+divIcone.style.justifyContent = 'space-evenly';
+divIcone.style.paddingLeft = '0.2rem';
+divIcone.style.marginLeft = '0.2rem';
+divIcone.style.position = 'relative';
+divIcone.style.top = '-0.5rem';
+quantityInCard.style.color = "black";
+quantityInCard.style.fontSize = 'x-small';
+quantityInCard.style.fontWeight = 'bold';
+iconeKanap.style.width = '1rem';
+iconeKanap.style.height = '0.5rem';
+
+function iconKanapOnCart(){
+  if(productInLocalStorage){
+    let quantitiesArrayForIcone = [];
+    for(let eachQuantity of productInLocalStorage){
+      let eachQuantityLS = parseInt(eachQuantity.quantity);
+      quantitiesArrayForIcone.push(eachQuantityLS);
+    }
+    totalQuantity = quantitiesArrayForIcone.reduce(reducer, 0);
+    divIcone.style.display = 'flex'; 
+    quantityInCard.innerHTML = Number(totalQuantity);  
+    iconeKanap.innerHTML = '<svg><path d="M 10.656 4.032 C 10.179 4.032 9.792 4.419 9.792 4.896 V 6.336 H 1.728 V 4.896 C 1.728 4.419 1.3412 4.032 0.864 4.032 S 0 4.419 0 4.896 v 3.456 C 0 8.5104 0.129 8.64 0.288 8.64 h 1.152 c 0.159 0 0.2718 -0.129 0.2718 -0.288 L 1.728 8.064 h 8.064 v 0.288 c 0 0.159 0.129 0.288 0.288 0.288 h 1.152 c 0.159 0 0.288 -0.129 0.288 -0.288 v -3.456 C 11.52 4.419 11.133 4.032 10.656 4.032 z M 2.304 4.896 V 5.76 h 6.912 V 4.896 c 0 -0.6953 0.4955 -1.2771 1.152 -1.4108 V 2.88 c 0 -1.2724 -1.0316 -2.304 -2.304 -2.304 H 3.4398 c -1.2724 0 -2.304 1.0316 -2.304 2.304 L 1.152 3.4848 C 1.809 3.6198 2.304 4.2012 2.304 4.896 z"></path></svg>';
+       
+  }else{
+    divIcone.style.display = 'none';
+    quantityInCard.style.display = 'none';
+    iconeKanap.style.display = 'none';
+    divIcone.style.border = 'none';
+  }
+}
+
+iconKanapOnCart();
+
+
+
 
 /*________RETRIEVE HTML ELEMENTS________*/
 const displayTotalPrice = document.getElementById('totalPrice');
@@ -30,6 +84,7 @@ function calculateAndDisplayTotalQuantity(){
   }
   const totalQuantity = quantitiesArray.reduce(reducer, 0); // ADD UP THEM TOGETHER TO GET THE TOTAL QUANTITY OF ALL THE CART
   displayTotalQuantity.innerHTML = Number(0 + totalQuantity); //DISPLAYS THE TOTAL QUANTITY
+  iconKanapOnCart();
 }
 
 /*________TOTAL PRICE________*/
@@ -47,15 +102,18 @@ let priceArray = []; //ARRAY OF EACH PRICE
       displayTotalPrice.innerHTML = 0;
     }
   })
+  iconKanapOnCart();
 }
 
 function removeFromBasket(product){ //DELETE PRODUCT FORM CART
   productInLocalStorage = productInLocalStorage.filter(function (elt) {return elt.color != product.dataset.color || elt.id != product.id} ); //FILTER TO GET THE RIGHT PRODUCT
   savingCart(); 
+  iconKanapOnCart();
 }
 
 function savingCart(){
   localStorage.setItem("product", JSON.stringify(productInLocalStorage));
+  iconKanapOnCart();
 }
 
 if(productInLocalStorage){ //IF LOCAL STORAGE IS -NOT- EMPTY
@@ -308,7 +366,7 @@ let emailValid = false;
 /*________FIRSTNAME________*/ 
 firstName.addEventListener('change', function(event){
   const firstNameValue = event.target.value;
-  if(/^[a-zA-Z ,.'-]+$/.test(firstNameValue)){ //IF VALID
+  if(/^[a-zA-ZÀ-ÿ-.,' ]*$/.test(firstNameValue)){ //IF VALID
     contact.firstName = firstNameValue; //PUSH THE VALUE ON THE OBJECT CONTACT
     firstNameErrorMsg.innerHTML = ''; //NO ERROR MESSAGE
     firstNameValid = true; //IF VALID, RETURN TRUE
@@ -321,7 +379,7 @@ firstName.addEventListener('change', function(event){
 /*________LASTNAME________*/ 
 lastName.addEventListener('change', function(event){
   const lastNameValue = event.target.value;
-  if(/^[a-zA-Z ,.'-]+$/.test(lastNameValue)){
+  if(/^[a-zA-ZÀ-ÿ-.,' ]*$/.test(lastNameValue)){
     contact.lastName = lastNameValue;
     lastNameErrorMsg.innerHTML = '';
     lastNameValid = true;
@@ -334,7 +392,7 @@ lastName.addEventListener('change', function(event){
 /*________ADRESS________*/ 
 address.addEventListener('change', function(event){
   const addressValue = event.target.value;
-  if(/^[#.0-9a-zA-Z\s,-]+$/.test(addressValue)){
+  if(/^[#.0-9a-zA-ZÀ-ÿ\s,-]+$/.test(addressValue)){
     contact.address = addressValue;
     addressErrorMsg.innerHTML = '';
     adressValid = true;
@@ -347,7 +405,7 @@ address.addEventListener('change', function(event){
 /*________CITY________*/ 
 city.addEventListener('change', function(event){
   const cityValue = event.target.value;
-  if(/^[a-zA-Z ,.'-]+$/.test(cityValue)){
+  if(/^[a-zA-ZÀ-ÿ ,.'-]+$/.test(cityValue)){
     contact.city = cityValue;
     cityErrorMsg.innerHTML = '';
     cityValid = true;
@@ -360,7 +418,7 @@ city.addEventListener('change', function(event){
 /*________EMAIL________*/ 
 email.addEventListener('change', function(event){
   const emailValue = event.target.value;
-  if(/^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+[@]+[a-zA-Z]+[.]+[a-zA-Z]+$/.test(emailValue)){
+  if(/^[a-zA-ZÀ-ÿ0-9.!#$%&'*+=?^_`{|}~-]+[@]+[a-zA-Z]+[.]+[a-zA-Z]+$/.test(emailValue)){
     contact.email = emailValue;
     emailErrorMsg.innerHTML = '';
     emailValid= true;
--- front/js/product.js
+++ front/js/product.js
@@ -77,6 +77,58 @@ document.getElementById("quantity").addEventListener('change', function(){
 let colorChoosed = '';
 let quantityChoosed = '';
 
+
+
+/*________CREATE AND STYLING ELEMENTS FOR SVG THAT DISPLAYS QUANTITY OF PRODUCTS ON CART________*/
+let reducer = (accumulator, currentValue) => accumulator + currentValue;
+const nav = document.querySelector('.limitedWidthBlock nav ul');
+let totalQuantity = ''
+const divIcone = document.createElement('div');
+nav.appendChild(divIcone);
+const quantityInCard = document.createElement('p');
+const iconeKanap = document.createElement('p');
+divIcone.appendChild(quantityInCard);
+divIcone.appendChild(iconeKanap);
+divIcone.style.alignSelf = 'center';
+divIcone.style.alignItems = 'center';
+divIcone.style.border = '0.1rem dotted #3498db';
+divIcone.style.borderRadius = '1rem';
+divIcone.style.height = '1.9rem';
+divIcone.style.width = '1.7rem';
+divIcone.style.justifyContent = 'space-evenly';
+divIcone.style.paddingLeft = '0.2rem';
+divIcone.style.marginLeft = '0.2rem';
+divIcone.style.position = 'relative';
+divIcone.style.top = '-0.5rem';
+quantityInCard.style.color = "black";
+quantityInCard.style.fontSize = 'x-small';
+quantityInCard.style.fontWeight = 'bold';
+iconeKanap.style.width = '1rem';
+iconeKanap.style.height = '0.5rem';
+
+function iconKanapOnCart(){
+  if(productInLocalStorage){
+    let quantitiesArrayForIcone = [];
+    for(let eachQuantity of productInLocalStorage){
+      let eachQuantityLS = parseInt(eachQuantity.quantity);
+      quantitiesArrayForIcone.push(eachQuantityLS);
+    }
+    totalQuantity = quantitiesArrayForIcone.reduce(reducer, 0);
+    divIcone.style.display = 'flex'; 
+    quantityInCard.innerHTML = Number(totalQuantity);  
+    iconeKanap.innerHTML = '<svg><path d="M 10.656 4.032 C 10.179 4.032 9.792 4.419 9.792 4.896 V 6.336 H 1.728 V 4.896 C 1.728 4.419 1.3412 4.032 0.864 4.032 S 0 4.419 0 4.896 v 3.456 C 0 8.5104 0.129 8.64 0.288 8.64 h 1.152 c 0.159 0 0.2718 -0.129 0.2718 -0.288 L 1.728 8.064 h 8.064 v 0.288 c 0 0.159 0.129 0.288 0.288 0.288 h 1.152 c 0.159 0 0.288 -0.129 0.288 -0.288 v -3.456 C 11.52 4.419 11.133 4.032 10.656 4.032 z M 2.304 4.896 V 5.76 h 6.912 V 4.896 c 0 -0.6953 0.4955 -1.2771 1.152 -1.4108 V 2.88 c 0 -1.2724 -1.0316 -2.304 -2.304 -2.304 H 3.4398 c -1.2724 0 -2.304 1.0316 -2.304 2.304 L 1.152 3.4848 C 1.809 3.6198 2.304 4.2012 2.304 4.896 z"></path></svg>';
+       
+  }else{
+    divIcone.style.display = 'none';
+    quantityInCard.style.display = 'none';
+    iconeKanap.style.display = 'none';
+    divIcone.style.border = 'none';
+  }
+}
+ 
+iconKanapOnCart();
+
+
 /*________CONFIRMATION PRODUCT ADDED ON CART________*/
 function notifProductAdded(){ 
   if(quantityChoosed == 1){
@@ -147,7 +199,7 @@ addButton.addEventListener('click',function(event){
 
     const resultFind = productInLocalStorage.find((el) => el.id === id && el.color === colorChoosed); //SEARCH IF PRODUCT IS ON CART
      
-    if(resultFind) { //IF THE PRODUCT IS ALREADY ON CART : UPDATE THE QUANTITY
+    if(resultFind) { //IF THE PRODUCT IS ALREADY ON CART : UPDATE THE QUANTITY IF QUANTIY IS NOT TOO HIGHT
       let quantityInLS = resultFind.quantity;
       let newQuantite = parseInt(resultFind.quantity + quantityChoosed );
       const maxQuantity = 100;
@@ -165,13 +217,15 @@ addButton.addEventListener('click',function(event){
       }
       if(newQuantite <= maxQuantity){ //UPDATE QUANTITY, CONFIRM PRODUCT ADDED AND SAVE CART
         resultFind.quantity = newQuantite;
-        notifProductAdded();
+        notifProductAdded();    
+        iconKanapOnCart();
         return localStorage.setItem('product', JSON.stringify(productInLocalStorage ));
       }  
 
     }else{ //IF THE PRODUCT IS NOT ON CART : PUSH THE ENTIRE ARTICLE ON CART
       productInLocalStorage.push(article);
-      notifProductAdded();
+      notifProductAdded();    
+      iconKanapOnCart();
       return localStorage.setItem('product', JSON.stringify(productInLocalStorage ));
     }
         
@@ -179,7 +233,8 @@ addButton.addEventListener('click',function(event){
     cart = [];
     cart.push(article);
     productInLocalStorage = cart;
-    notifProductAdded();
+    notifProductAdded();    
+    iconKanapOnCart();
     return localStorage.setItem("product", JSON.stringify(cart));
   }
 
